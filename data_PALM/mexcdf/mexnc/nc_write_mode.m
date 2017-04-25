4f8251b8b9742c88d9eba04d58a751e76ab54838
Sjoerd Boersma - 3ME <sboersma3@tudelft.net>
2017-04-25 15:20:11 +0200

    removed mexcdf

--- data_PALM/mexcdf/mexnc/nc_write_mode.m
+++
@@ -1,8 +0,0 @@
-function mode = nc_write_mode()
-% NC_WRITE_MODE:  returns integer mnemonic for NC_WRITE
-%
-% USAGE:  mode = nc_write_mode;
-mode = 1;
-return
-
-
--- data_PALM/mexcdf/mexnc/src/r2008a.mk
+++
@@ -1,6 +0,0 @@
-all:
-	mex -g -v -f ./mexopts.R2008a.sh -output vanilla_mexnc mexgateway.c netcdf2.c netcdf3.c common.c 
-	mv *.mex*  ../private
-
-clean:
-	rm -f *.o mexnc.mex*
