244af38bc0568c3536628285ca2ecdb0976206c1
Sjoerd Boersma <sboersma3@tudelft.net>
2017-08-25 11:28:33 +0200

    update
    Former-commit-id: d27d967b4f3770175ec598f6598b9637963172dd
    Former-commit-id: 8ba0ec51ec70cb885bc7fa98d6d8aa47689cd284

--- WFSim.m
+++ WFSim.m
@@ -21,11 +21,11 @@ options.exportPressures= ~options.Projection;    % Calculate pressure fields
   
 %Wp.name             = 'WP_CPUTime';      % Meshing name (see "\bin\core\meshing.m")
 %Wp.name             = 'WP_CPUTime';
-Wp.name             = 'ThreeTurbine_Ampc';
+Wp.name             = 'SingleTurbine_50x50_lin';
 
 Wp.Turbulencemodel  = 'WFSim3';
 
-Animate       = 1;                      % Show 2D flow fields every x iterations (0: no plots)
+Animate       = 100;                      % Show 2D flow fields every x iterations (0: no plots)
 plotMesh      = 0;                      % Show meshing and turbine locations
 conv_eps      = 1e-6;                   % Convergence threshold
 max_it_dyn    = 1;                      % Maximum number of iterations for k > 1
