65f9023c10b03e14595cf35d993118af86f3b4eb
Sjoerd Boersma <sboersma3@tudelft.net>
2017-08-30 13:53:36 +0200

    update to master
    Former-commit-id: 73efe98f8211a0c2ec3fde871895d0c4bf6b5936
    Former-commit-id: 486c9d60735cc0ede1026d25c76857475d5a90e7

--- WFSim.m
+++ WFSim.m
@@ -62,39 +62,6 @@ clear; clc; close all; %
 %     - If you cannot solve your problems, reach out on the Github.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-<<<<<<< HEAD
-% Types of systems when Projection=0:
-% Linearversion = 0;
-% sys.A * x_{k+1}  = sys.b(x_k,u_k)
-% sys.A * x_{k+1}  = sys.M * x_k + sys.m(x_k,u_k)
-% Linearversion = 1;
-% sys.A * dx_{k+1} = sys.Al * dx_k + sys.bl(du_k)
-% sys.A * dx_{k+1} = sys.Al * dx_k + sys.Bl * du_k
-
-clear; clc; close all;
-
-WFSim_addpaths
-
-%% Initialize script
-options.Projection     = 0;                      % Use projection (true/false)
-options.Linearversion  = 0;                      % Provide linear variant of WFSim (true/false)
-options.exportLinearSol= 0;                      % Calculate linear solution of WFSim
-options.Derivatives    = 0;                      % Compute derivatives
-options.startUniform   = 0;                      % Start from a uniform flowfield (true) or a steady-state solution (false)
-options.exportPressures= ~options.Projection;    % Calculate pressure fields
-  
-%Wp.name             = 'WP_CPUTime';      % Meshing name (see "\bin\core\meshing.m")
-%Wp.name             = 'WP_CPUTime';
-Wp.name             = '2turb_adm';
-
-Wp.Turbulencemodel  = 'WFSim4';
-
-Animate       = 0;                      % Show 2D flow fields every x iterations (0: no plots)
-plotMesh      = 0;                      % Show meshing and turbine locations
-conv_eps      = 1e-6;                   % Convergence threshold
-max_it_dyn    = 1;                      % Maximum number of iterations for k > 1
-=======
-
 
 %% Define script settings
 Wp.name      = 'apc_9turb_alm_turb';    % Choose which scenario to simulate. See 'bin/core/meshing.m' for the full list.
@@ -106,7 +73,6 @@ scriptOptions.exportLinearSol   = 0;        % Calculate linear solution of WFSim
 scriptOptions.Derivatives       = 0;        % Compute derivatives, useful for predictive control (bool).     Default: false.
 scriptOptions.startUniform      = 1;        % Start with a uniform flowfield (true) or with fully developed wakes (false).
 scriptOptions.exportPressures   = ~scriptOptions.Projection;   % Calculate pressure fields. Default: '~scriptOptions.Projection'
->>>>>>> master
 
 % Convergence settings (recommended: leave default)
 scriptOptions.conv_eps          = 1e-6;     % Convergence threshold. Default: 1e-6.
@@ -117,30 +83,11 @@ else
     scriptOptions.max_it = 50;              % Maximum n.o. of iterations for k == 1, when startUniform = 0.
 end
 
-<<<<<<< HEAD
-%profile on
-
-%vid = VideoWriter('flow.avi');
-%open(vid);
-
-inc         = 1;
-meanCPUTime = zeros(inc,2);
-for ll=1:inc
-   
-    Wp.ll = ll;
-
-% WFSim general initialization script
-[Wp,sol,sys,Power,CT,a,Ueffect,input,B1,B2,bc] = InitWFSim(Wp,options,plotMesh);
-
-if Animate > 0
-    scrsz = get(0,'ScreenSize');
-=======
 % Display and visualization settings
 scriptOptions.printProgress     = 1;    % Print progress in cmd window every timestep. Default: true.
 scriptOptions.printConvergence  = 0;    % Print convergence values every timestep.     Default: false.
 scriptOptions.Animate           = 10;   % Plot flow fields every [X] iterations (0: no plots). Default: 10.
 scriptOptions.plotMesh          = 1;    % Plot mesh, turbine locations, and print grid offset values. Default: false.
-
 
 
 %% Script core functions
@@ -151,72 +98,10 @@ run('WFSim_addpaths.m');                    % Add essential paths to MATLABs env
 sol_array = {}; % Create empty array to save 'sol' to at each time instant
 CPUTime   = zeros(Wp.sim.NN,1); % Create empty matrix to save CPU timings
 if scriptOptions.Animate > 0 % Create empty figure if Animation is on
->>>>>>> master
     hfig = figure('color',[0 166/255 214/255],'units','normalized','outerposition',...
            [0 0 1 1],'ToolBar','none','visible', 'on');
 end
 
-<<<<<<< HEAD
-CPUTime = zeros(Wp.sim.NN,1);
-%% Loop 
-for k=1:Wp.sim.NN
-    tic
-    it        = 0;
-    eps       = 1e19;
-    epss      = 1e20;
-    sol.uk    = sol.u; 
-    sol.vk    = sol.v;
-    u(:,:,k)  = sol.u;
-    v(:,:,k)  = sol.v;
-    
-    while ( eps>conv_eps && it<max_it && eps<epss );
-        it   = it+1;
-        epss = eps;
-        
-        if k>1 
-            max_it = max_it_dyn; 
-        end
-        
-        [sys,Power(:,k),Ueffect(:,k),a(:,k),CT(:,k)] = ...
-                    Make_Ax_b(Wp,sys,sol,input{k},B1,B2,bc,k,options);              % Create system matrices
-        [sol,sys] = Computesol(sys,input{k},sol,k,it,options);                      % Compute solution
-        [sol,eps] = MapSolution(Wp.mesh.Nx,Wp.mesh.Ny,sol,k,it,options);            % Map solution to field
-        
-        display(['k ',num2str(k,'%-1000.1f'),', It ',num2str(it,'%-1000.0f'),]);
-    end
-    CPUTime(k) = toc;
-    
-    %if k==50
-        %Wp.site.u_Inf           = 12;
-        %[B1,B2,bc]              = Compute_B1_B2_bc(Wp);
-        %sol.u(1:2,1:Wp.mesh.Ny) = Wp.site.u_Inf;
-        %Wp.site.v_Inf           = 2;    
-        %sol.v(1:2,1:Wp.mesh.Ny) = Wp.site.v_Inf;
-    %end
-    
-    if Animate > 0
-        if ~rem(k,Animate)
-            Animation; 
-        end; 
-    end; 
-    
-    %fname = sprintf('D:/sboersma3/My Documents/MATLAB/WFSim/data_WFSim/Change axial induction/%d',k);
-    %fname = sprintf('D:/sboersma3/My Documents/MATLAB/WFSim/data_WFSim/Change yaw/%d',k);
-    %save(fname,'sol','Wp','k','input')
-    
-    %frame = getframe(gcf);
-    %writeVideo(vid,frame);
-    q(k,:) = sys.q;
-end
-%close(vid);
-
-meanCPUTime(ll,:) = [mean(CPUTime(2:end)) size(sol.x,1)];
-%meanCPUTimeWFSim3 = meanCPUTime; save('WFSim3extended.mat','meanCPUTimeWFSim3extended')
-end
-%profile viewer
-
-disp('Completed simulations.');
-=======
 % Performing forward time propagations
 disp(['Performing ' num2str(Wp.sim.NN) ' forward simulations..']);
 while sol.k < Wp.sim.NN
@@ -241,4 +126,3 @@ while sol.k < Wp.sim.NN
     end; 
 end;
 disp(['Completed ' num2str(Wp.sim.NN) ' forward simulations. Average CPU time: ' num2str(mean(CPUTime)*10^3,3) ' ms.']);
->>>>>>> master
