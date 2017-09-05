e343757217b8638a038842d5c059b1bda5025fac
Bartdoekemeijer <B.M.Doekemeijer@tudelft.nl>
2017-09-05 10:35:29 +0200

    Add PALM APC case

--- bin/core/meshing.m
+++ bin/core/meshing.m
@@ -61,6 +61,19 @@ switch lower(scenarioName)
         m          = 4;          % Turbulence model gridding property        
         n          = 2;          % Turbulence model gridding property
         
+    case lower('apc_9turb_adm_noturb')
+        [meshFn,measurementFn] = downloadLESdata( WFSimfolder, lower(scenarioName) ); % Download files
+        load(meshFn);            % Load the LES meshing file
+        Drotor     = Drotor(1);  % WFSim only supports a uniform Drotor for now
+        powerscale = .90;        % Turbine power scaling
+        forcescale = 1.6;        % Turbine force scaling
+        p_init     = 0.0;        % Initial values for pressure terms (Pa)
+        turbul     = true;       % Use mixing length turbulence model (true/false)        
+        turbModel  = 'WFSim3';   % Turbulence model of choice   
+        lmu        = 0.3;        % Mixing length in x-direction (m)
+        mu         = 0.0;        % Dynamic flow viscosity
+        m          = 4;          % Turbulence model gridding property        
+        n          = 2;          % Turbulence model gridding property        
         
     % Wind farms for which SOWFA data is available
     case lower('2turb_alm_noturb')
