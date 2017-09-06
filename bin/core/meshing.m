ee8c8fbdfc82d5f3db68c86caa2befd55a04e7fd
Bartdoekemeijer <B.M.Doekemeijer@tudelft.nl>
2017-09-06 16:30:24 +0200

    Update meshing.m with better tuning settings. Also, merge the two 2turb_adm cases, since they were almost identical in turbulence, so same model parameters apply.

--- bin/core/meshing.m
+++ bin/core/meshing.m
@@ -19,37 +19,24 @@ WFSimfolder = binloc(1:strbslash(end)+6);
 switch lower(scenarioName)    
     
     % Wind farms for which PALM data is available        
-    case lower('2turb_adm_noturb') 
+    case{lower('2turb_adm_turb'),lower('2turb_adm_noturb')}
         [meshFn,measurementFn] = downloadLESdata( WFSimfolder, lower(scenarioName) ); % Download files
         load(meshFn);            % Load the LES meshing file
         Drotor     = Drotor(1);  % WFSim only supports a uniform Drotor for now
         powerscale = 1.0;        % Turbine power scaling
-        forcescale = 1.8;        % Turbine force scaling
+        forcescale = 1.7;        % Turbine force scaling
         p_init     = 0.0;        % Initial values for pressure terms (Pa)
         turbul     = true;       % Use mixing length turbulence model (true/false)        
         turbModel  = 'WFSim3';   % Turbulence model of choice
-        lmu        = .3;         % Mixing length in x-direction (m)
+        lmu        = 0.45;       % Mixing length in x-direction (m)
         mu         = 0*18e-5;    % Dynamic flow viscosity        
-        m          = 5;          % Turbulence model gridding property        
-        n          = 1;          % Turbulence model gridding property
-        
-        % Tuning notes '2turb_adm_noturb' (Sep 5th, 2017): 
-        % Ranges: lmu= 0.1:0.1:2.0, f = 0.8:0.1:2.0, m = 1:8, n = 1:4
-        % Note:   gridsearched with wrong Drotor, corrected afterwards.
-        
-    case lower('2turb_adm_turb')
-        [meshFn,measurementFn] = downloadLESdata( WFSimfolder, lower(scenarioName) ); % Download files
-        load(meshFn);            % Load the LES meshing file
-        Drotor     = Drotor(1);  % WFSim only supports a uniform Drotor for now
-        powerscale = .85;        % Turbine power scaling
-        forcescale = 1.6;        % Turbine force scaling
-        p_init     = 0.0;        % Initial values for pressure terms (Pa)
-        turbul     = true;       % Use mixing length turbulence model (true/false)        
-        turbModel  = 'WFSim3';   % Turbulence model of choice   
-        lmu        = 0.3;        % Mixing length in x-direction (m)
-        mu         = 0.0;        % Dynamic flow viscosity
         m          = 1;          % Turbulence model gridding property        
         n          = 4;          % Turbulence model gridding property
+                         
+        % Tuning notes '2turb_adm_turb', '2turb_adm_noturb' (Sep 6th, 2017): 
+        % Ranges: lmu= 0.1:0.1:2.0, f = 0.8:0.1:2.0, m = 1:8, n = 1:4
+        % Note:   Gridsearched multiple ways, should be very reliable now.
+        % Results for both cases are so similar that they have been merged.
         
     case lower('2turb_yaw_adm_noturb')
         [meshFn,measurementFn] = downloadLESdata( WFSimfolder, lower(scenarioName) ); % Download files
@@ -100,7 +87,6 @@ switch lower(scenarioName)
 
         % Tuning notes '2turb_alm_noturb' (Sep 5th, 2017): 
         % Ranges: lmu= 0.1:0.1:2.0, f = 0.8:0.1:2.0, m = 1:8, n = 1:4
-        % Note:   Seems to reach m/n limits. Could be improved?
                   
     case lower('2turb_alm_turb')
         [meshFn,measurementFn] = downloadLESdata( WFSimfolder, lower(scenarioName) ); % Download files
@@ -116,6 +102,9 @@ switch lower(scenarioName)
         m          = 1;          % Turbulence model gridding property        
         n          = 3;          % Turbulence model gridding property
 
+        % Tuning notes '2turb_alm_turb' (Sep 6th, 2017): 
+        % Ranges: lmu= 0.1:0.1:2.0, f = 0.8:0.1:2.0, m = 1:8, n = 1:4
+         
     case lower('apc_9turb_alm_turb')
         [meshFn,measurementFn] = downloadLESdata( WFSimfolder, lower(scenarioName) ); % Download files
         load(meshFn);             % Load the LES meshing file
