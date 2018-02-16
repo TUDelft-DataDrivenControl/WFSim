2ed79e8a79dc837f81caa707c85db83ef46d07b7
Bart Doekemeijer <B.M.Doekemeijer@tudelft.nl>
2018-02-16 17:45:29 +0100

    Add constructLmu function that is universal to any farm, and includes possibilities for changing wind directions

--- bin/core/ConstructLmu.m
+++ bin/core/ConstructLmu.m
@@ -1,36 +1,45 @@
-function lmu = ConstructLmu( Wp )
+function lm=constructLmu(x_IF,y_IF,WD,xTurbs,yTurbs,D,d_lower,d_upper,lm_slope)
+    % Check inputs
+    if d_upper <= d_lower
+        error('Make sure your upper bound is larger than your lower bound on the Lmu turbulence model.');
+    end
 
-Nx      = Wp.mesh.Nx;
-Ny      = Wp.mesh.Ny;
-xline   = Wp.mesh.xline;
-yline   = Wp.mesh.yline;
-d       = Wp.site.m+15; % 0< d < xline(i+1)- xline(i)
-dprime  = Wp.site.n;
-max_lmu = Wp.site.lmu; % = ls*(d-dprime)
+    lm = zeros(size(x_IF));    
+    
+    % Rotation vector
+    rotWD = [cos(WD) -sin(WD);sin(WD) cos(WD)];
+    
+    % Add mixing length to the field for every turbine
+    for iT = 1:length(xTurbs)
+        xy_WF = [x_IF(:)-xTurbs(iT) y_IF(:)-yTurbs(iT)]*rotWD;
+        x_WF  = reshape(xy_WF(:,1),size(x_IF,1),size(x_IF,2));
+        y_WF  = reshape(xy_WF(:,2),size(y_IF,1),size(y_IF,2));
 
-uniqueXline = sort(unique(xline));
-for kk=1:length(yline);Yline(kk,:)=yline{kk};end
-uniqueYline = unique(sort(Yline),'rows');
+        % Determine turbine-added mixing length
+        lm = lm + Lmu_2D_WF(x_WF,y_WF,D,d_lower,d_upper,lm_slope);
+    end
+    
+    % % Plot results
+    % clf; surf(X,Y,lm);
+    % axis equal tight
+    % xlabel('x (m)');
+    % ylabel('y (m)');
+    % title('Lmu plot (m)');
+    % drawnow()
 
-% lmu in x-direction
-lmux    = [];
-for kk=1:length(uniqueXline)-1
-    a                        = uniqueXline(kk+1)-uniqueXline(kk);
-    temp                     = zeros(1,a);
-    temp(dprime+1:d-dprime)  = linspace(0,max_lmu,d-2*dprime);
-    lmux                     = [lmux temp];
+    % 2D Lmu profile for single turbine case at (x,y)=(0,0)
+    %    this is the classical profile with sharp corners
+    function lm=Lmu_2D_WF(x,y,D,d_lower,d_upper,lm_slope)
+        lm       = zeros(size(x));
+        indx     = ((x > d_lower) & (x < d_upper) & (y <= D/2) & (y > -D/2));
+        lm(indx) = (x(indx)-d_lower).*lm_slope;
+    end
+
+%     % 2D Lmu profile for single turbine case at (x,y)=(0,0)
+%     %    this is the quadratic shape with rounded corners and expanding width
+%     function lm=Lmu_2D_WF(x,y,D,d_lower,d_upper,lm_slope)
+%         lm       = zeros(size(x));
+%         indx = ((x > d_lower) & (x < d_upper) & (abs(y) < sqrt(((x-d_lower).*lm_slope)/k_2)));
+%         lm(indx) = (x(indx)-d_lower)*lm_slope - (y(indx).^2)*k_2;
+%     end
 end
-
-lmux = [zeros(1,uniqueXline(1)) lmux zeros(1,dprime) linspace(0,max_lmu,Nx-uniqueXline(end)-dprime)];
-
-% lmu in y-direction
-lmuy    = zeros(1,Ny);
-for kk=1:size(uniqueYline,1)
-    lmuy(uniqueYline(kk,:)) = 1;
-end
-
-% lmu in x- and y-direction
-lmu  = (repmat(lmux',1,Ny).*repmat(lmuy,Nx,1))*0.5*Wp.turbine.Drotor; 
-
-end
-
