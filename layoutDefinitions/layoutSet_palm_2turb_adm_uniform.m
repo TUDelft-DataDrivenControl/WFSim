edd79dbbc66e65fdef685b08382136e2b2cbef7a
Bartdoekemeijer <B.M.Doekemeijer@tudelft.nl>
2018-11-05 11:59:15 +0100

    Retune the PALM 2-turbine case by hand

--- layoutDefinitions/layoutSet_palm_2turb_adm_uniform.m
+++ layoutDefinitions/layoutSet_palm_2turb_adm_uniform.m
@@ -4,13 +4,13 @@ Wp = struct('description','2 NREL 5MW turbines case, uniform inflow, based on a 
 
 Wp.sim = struct(...
     'h',1.0,... % timestep (s)
-    'startUniform',true ... % Start from a uniform flow field (T) or from a fully developed waked flow field (F).
+    'startUniform',false ... % Start from a uniform flow field (T) or from a fully developed waked flow field (F).
     );
 
 Wp.turbine = struct(...
     'Crx',[180 936],... % X-coordinates of turbines (m)
-    'Cry',[250 250],... % Y-coordinates of turbines (m)
-    'Drotor',120.0,... % Rotor diameter (m), note that WFSim only supports a uniform Drotor for now
+    'Cry',[255 255],... % Y-coordinates of turbines (m)
+    'Drotor',110.0,... % Rotor diameter (m), note that WFSim only supports a uniform Drotor for now
     'powerscale',1.0,... % Turbine power scaling
     'forcescale',1.7 ... % Turbine force scaling
     );
@@ -19,7 +19,7 @@ Wp.site = struct(...
     'u_Inf',7.986,... % Initial long. wind speed in m/s
     'v_Inf',0.0,... % Initial lat. wind speed in m/s
     'p_init',0.0,... % Initial values for pressure terms (Pa)
-    'lm_slope',0.1,... % Mixing length in x-direction (m)
+    'lm_slope',0.01,... % Mixing length in x-direction (m)
     'd_lower',73.3,... % Turbulence model gridding property
     'd_upper',601.9,... % Turbulence model gridding property
     'Rho',1.20 ... % Air density
@@ -33,8 +33,8 @@ Wp.mesh = struct(...
     );
 
 
-% Tuning notes '2turb_adm_turb', '2turb_adm_noturb' (Sep 6th, 2017):
-% Ranges: lmu= 0.1:0.1:2.0, f = 0.8:0.1:2.0, m = 1:8, n = 1:4
-% Note:   Gridsearched multiple ways, should be very reliable now.
-% Results for both cases are so similar that they have been merged.
+% Tuned by hand on November 5, 2018. There may be some discrepancies
+% at the rotor/wake edge due to mesh mismatches. This is unavoidable and
+% would be significantly less noticeable under turbulence and mixing. In
+% general, the dynamics are very well captured.
 end
